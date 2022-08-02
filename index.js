f6819e7e91f852ab8be097bc3fe6b63bc27f7257
Fran <bygarcia99@gmail.com>
2022-08-02 15:05:51 +0200

    segundo commit

--- index.js
+++ index.js
@@ -1,68 +1,95 @@
+/*const*/
+/*--------------------------------------------------------------------------------------*/
+
 //Almacenar lista de palabras posibles en un objeto / array.
-const words = ["beber", "padre", "playa", "poeta", "rosas"];
+const words = ["padre", "beber", "playa", "poeta", "rosas"];
 
-//-> Al inicio del programa, seleccionar una palabra de manera aleatoria de la
-//lista para adivinar.
-const randomWord = words[Math.floor(Math.random() * words.length)];
-//console.log("la palabra es:");
-//console.log(randomWord);
+//const maxAttempt = 6;
 
-//Permitir al usuario que introduzca una palabra a través de un input y,
-//tras pulsar un botón, que salga un alert anunciando si ha acertado o
-//no (con una comparación de palabras exacta).
+let game = {
+  randomWord: "",
+  validAttempts: [],
+  allWords: [],
+};
 
-function check(e) {
-  const playWord = document.getElementById("accept");
-  //console.log(playWord.value);
-  if (playWord.value === randomWord) {
+/*functions*/
+/*--------------------------------------------------------------------------------------*/
+
+function start() {
+  //-> Al inicio del programa, seleccionar una palabra de manera aleatoria de la
+  //lista para adivinar.
+  game.randomWord = words[Math.floor(Math.random() * words.length)];
+  game.validAttempts = [];
+  game.allWords = [];
+}
+
+function check() {
+  event.preventDefault();
+  //recupero la informacion del input
+  const gameAttempt = document.querySelector("#accept").value;
+  //añadiendo el valor del intento dentro de la lista
+  addValueToList(gameAttempt);
+  const valid = validWords(gameAttempt);
+
+  if (!valid) {
+    return;
+  }
+
+  if (gameAttempt === game.randomWord) {
     alert("has acertado!");
   } else {
     alert("no has acertado");
   }
-  e.preventDefault();
-
-  addValueToList();
 
   document.querySelector("#accept").value = "";
 }
-
-document.getElementById("write").onsubmit = check;
-
 //En este punto hay que hacer que se muestren en el listado inferior todos
 //los intentos de palabras del usuario.
-function addValueToList() {
-  const lists = document.getElementById("accept");
-  //console.log(lists.value);
+function addValueToList(item) {
   /* Crear un elemento de lista */
   let listItem = document.createElement("li");
-  listItem.textContent = lists.value;
+  listItem.textContent = item;
 
   /* Vinculando item de lista "li" a elemento de lista "ul" */
   const addToList = document.getElementById("list");
   addToList.appendChild(listItem);
 }
 
+function validWords(attempt) {
+  if (attempt === "") {
+    console.log(attempt);
+    alert("No puedes dejar el campo vacio");
+    return false;
+  }
+  if (attempt.length > 5) {
+    alert("la palabra no puede tener mas de 5 letras");
+    return false;
+  }
+  if (attempt.length < 5) {
+    alert("la palabra no puede tener menos de 5 letras");
+    return false;
+  }
+  return true;
+}
+
+function resetGame() {
+  //Que se elija una nueva palabra, borrar la lista de intentos
+  game.randomWord = words[Math.floor(Math.random() * words.length)];
+  document.getElementById("list").innerHTML = "";
+}
+
+/*Events*/
+/*--------------------------------------------------------------------------------------*/
+
+document.getElementById("write").onsubmit = check;
 document.getElementById("tries").onsubmit = addValueToList;
+document.getElementById("reset").onclick = resetGame;
 
-//Crear una estructura de datos que permita almacenar los datos referentes
-//a la partida que estamos jugando. Hay que pensar cómo almacenar la
-//palabra a acertar y los intentos no válidos realizados hasta el momento.
+//Eliminar valores de las estructuras internas de datos.Fijate en el reset que ha hecho Marcos(reinicio de juego)
 
-//Contabilizar el número de intentos y no dejar realizar más de 6.
-//Cuando se llegue al máximo, avisar con un “alert” de que ya no se puede
-//jugar más.
+/*Run*/
+/*--------------------------------------------------------------------------------------*/
 
-//Validar los intentos del usuario. No dejar que se introduzcan cadenas
-//vacías, números o palabras que no tengan una longitud exacta de 5
-//posiciones.
-//Mostrar avisos personalizados a través de “alerts” cuando:
-//El texto contenga números
-//El texto sea de longitud menor a cinco caracteres
-//El texto sea de longitud mayor a cinco caracteres
-//A pesar de las validaciones, se deberán seguir mostrando en la lista
-//inferior todos los intentos del usuario.
+start();
 
-//Crear y dar funcionalidad al botón de “Resetear partida” y hacer que el
-//tablero y las listas de palabras en pantalla se muestren como al inicio
-//(vacias)
-//Eliminar valores de las estructuras internas de datos.
+console.log(game);
